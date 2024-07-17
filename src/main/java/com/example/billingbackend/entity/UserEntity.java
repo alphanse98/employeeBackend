35d2fe043b700ad1c867345776dececf51127b15
alphanse98 <alphanse98@gmail.com>
2024-07-17 16:18:26 +0530

    api bug

--- src/main/java/com/example/billingbackend/entity/UserEntity.java
+++ src/main/java/com/example/billingbackend/entity/UserEntity.java
@@ -22,6 +22,9 @@ public class UserEntity {
     @Column(nullable = false)
     private String password;
 
+    @Column(nullable = false)
+    private String email;
+
 //    @Column(nullable = false)
 //    private String role;
 }
--- src/main/java/com/example/billingbackend/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.billingbackend.repository;
-
-import com.example.billingbackend.entity.EmployeeEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<EmployeeEntity,Long> {
-}
